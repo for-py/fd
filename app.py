ba0b27b7a8e482980f0da799b7e07f6e2fef4697
yzh <y18289743718@gmail.com>
2020-01-05 14:48:20 +0800

    init

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask
+from flask_script import Manager, Shell
+
+app = Flask(__name__)
+
+manager = Manager(app)
+
+
+# 为shell自动导入模块和相关类
+def make_shell_context():
+    return dict(app=app)
+
+
+manager.add_command('shell', Shell(make_context=make_shell_context))
+
+
+# 定义路由和视图函数
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    return 'hello, world!'
+
+
+if __name__ == '__main__':
+    manager.run()
